0f2919379f1011a8b5f50370898a01011d038218
oksana.labeiko <oksana.labeiko@itexus.com>
2021-10-25 18:34:08 +0300

    removed

--- js/script.js
+++
@@ -1,9 +0,0 @@
-menu.onclick = function myFunction() {
-	var x = document.getElementById('navMenu');
-
-	if (x.className === "nav-menu") {
-		x.className += " responsive";
-	} else {
-		x.className = "nav-menu";
-	}
-}
